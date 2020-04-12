7b90c3c80ba6f032677d6c0e22b09bc211833196
ramkoh <raman.a.kohli@gmail.com>
2020-04-12 12:53:09 +0100

    json format for cucncumber report

--- src/test/java/com/edu/postgrad/game/teams/selenium/StepsDefTest.java
+++ src/test/java/com/edu/postgrad/game/teams/selenium/StepsDefTest.java
@@ -5,7 +5,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/cucumber" , glue = "com.edu.postgrad.game.teams.selenium")
+@CucumberOptions(features = "src/test/resources/cucumber",
+        glue = "com.edu.postgrad.game.teams.selenium",
+        format = {"json:target/cucumber.json"})
 public class StepsDefTest {
 
 
